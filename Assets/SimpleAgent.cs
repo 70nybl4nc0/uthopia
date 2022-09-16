2d18364de3ac14d20b68e9ede43bda342eff3a9c
Tony Blanco <70nybl4nc0@gmail.com>
2022-09-16 15:36:15 -0400

    first commit

---
+++ Assets/SimpleAgent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class SimpleAgent : Agent
+{
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        print(actions);
+        base.OnActionReceived(actions);
+    }
+}
